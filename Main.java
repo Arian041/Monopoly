9f283b05cd75a26784b2fa2df2f94dadf4bee029
Arian041 <leo.chauchet1812@gmail.com>
2026-03-19 14:10:00 +0000

    classe des

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
         public static void main(String[] args) {
-         System.out.println("hello world !");
-        Plateau p1 = new Plateau();
+          System.out.println("hello world !");
+          Plateau p1 = new Plateau();
   }
 
 }
