990ee6392f5a8da2a9ece7ca59e45d978402fc7d
Arian041 <leo.chauchet1812@gmail.com>
2026-04-01 11:43:00 +0000

    classe case

---
+++ Case.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Case {
+
+    private ArrayList<Case> cases;
+
+    public Case() {
+        this.cases = new ArrayList<>();
+    }
+
+    public void ajouterCase(Case c) {
+        cases.add(c);
+    }
+
+    public Case getCase(int index) {
+        if (index >= 0 && index < cases.size()) {
+            return cases.get(index);
+        }
+        return null; // ou lancer une exception
+    }
+
+    public int getNombreCases() {
+        return cases.size();
+    }
+
+    
+
+
+
+
+
+}
+
+
